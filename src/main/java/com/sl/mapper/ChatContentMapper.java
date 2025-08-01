3982f7d29bebc1f868138986f68725356ce92e9a
sunlong43793390 <sunlong43793390@163.com>
2025-08-01 14:58:05 +0800

    update chatview page

---
+++ src/main/java/com/sl/mapper/ChatContentMapper.java
@@ -0,0 +1,29 @@
+package com.sl.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.sl.entity.ChatContent;
+import com.sl.entity.ChatContentExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface ChatContentMapper extends BaseMapper<ChatContent> {
+    int deleteByExample(ChatContentExample example);
+
+    int deleteByPrimaryKey(@Param("userId") String userId, @Param("sessionId") String sessionId);
+
+    int insert(ChatContent record);
+
+    int insertSelective(ChatContent record);
+
+    List<ChatContent> selectByExampleWithBLOBs(ChatContentExample example);
+
+    List<ChatContent> selectByExample(ChatContentExample example);
+
+    ChatContent selectByPrimaryKey(@Param("userId") String userId, @Param("sessionId") String sessionId);
+
+    int updateByPrimaryKeySelective(ChatContent record);
+
+    int updateByPrimaryKeyWithBLOBs(ChatContent record);
+
+    int updateByPrimaryKey(ChatContent record);
+}
